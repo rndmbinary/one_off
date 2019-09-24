6ff09adfde91339f0e735310724533022713bdaf
rndmbinary <19554693+rndmbinary@users.noreply.github.com>
2019-09-24 13:26:25 -0500

    Update Code

--- auto_refresh/auto_refresh.js
+++ auto_refresh/auto_refresh.js
@@ -1,8 +1,8 @@
 //For Browser Consoles Only
 
 javascript: document.getElementsByTagName("body")[0].innerHTML = "<iframe id=\"refreshFrame\" src=\""
-    + window.location.toString() + "\" style=\"position: absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;\"> 
-    + <\/iframe>";
+    + window.location.toString() + "\" style=\"position: absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;\">" 
+    + "<\/iframe>";
 
 reloadTimer = setInterval(function () {
     document.getElementById("refreshFrame").src = document.getElementById("refreshFrame").src
