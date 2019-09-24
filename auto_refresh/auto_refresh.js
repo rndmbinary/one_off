556405d4736fd1b92069e53b1c1951f8c8758bc7
rndmbinary <19554693+rndmbinary@users.noreply.github.com>
2019-09-24 13:25:39 -0500

    First Commit for Auto_Refresh

---
+++ auto_refresh/auto_refresh.js
@@ -0,0 +1,9 @@
+//For Browser Consoles Only
+
+javascript: document.getElementsByTagName("body")[0].innerHTML = "<iframe id=\"refreshFrame\" src=\""
+    + window.location.toString() + "\" style=\"position: absolute; top:0; left:0; right:0; bottom:0; width:100%; height:100%;\"> 
+    + <\/iframe>";
+
+reloadTimer = setInterval(function () {
+    document.getElementById("refreshFrame").src = document.getElementById("refreshFrame").src
+}, 180000)
